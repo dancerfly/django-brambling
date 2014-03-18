03e21fa403a7542212b9fdb938be0287874fe4d9
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-03-18 01:11:47 -0700

    Added basic bootstrap styles from argus.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Gemfile for Ruby dependencies
+# Currently we're only using Ruby for CSS preprocessing (ie., sass/compass/bootstrap-sass)
+source 'https://rubygems.org'
+gem 'sass'
+gem 'compass'
+gem 'bootstrap-sass'
---
+++ brambling/config.rb
@@ -0,0 +1,10 @@
+# Configuration file for Compass
+
+require 'bootstrap-sass'
+css_dir = "static/brambling/css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
