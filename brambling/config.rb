0887f0115fb57078c476cda463122c947117dc8b
Harris Lapiroff <harris@chromamine.com>
2014-04-06 13:34:15 -0400

    Fixed compass paths. Updated Ruby deps. Included glyphicons.

--- brambling/config.rb
+++ brambling/config.rb
@@ -3,8 +3,9 @@
 require 'bootstrap-sass'
 css_dir = "static/brambling/css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "static/brambling/images"
+javascripts_dir = "static/brambling/js"
+fonts_dir = "static/brambling/fonts"
 relative_assets = true
 line_comments = false
 preferred_syntax = :sass
