19df70ef9a9922753e68227a56327a0521fe9b3f
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-04-18 20:07:44 -0700

    Removed styles/js/html that have been moved to zenaida.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'littleweaver_forms',
+    'zenaida',
     'floppyforms',
 )
 
