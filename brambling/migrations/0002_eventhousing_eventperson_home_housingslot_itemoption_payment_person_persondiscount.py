8683ad5963b3832ce07314ce6978fec57519a4f5
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-04-25 10:51:27 -0700

    Added explicit contenttypes dependency for migration 0002. Workaround for django bug #22518. https://code.djangoproject.com/ticket/22518

--- brambling/migrations/0002_eventhousing_eventperson_home_housingslot_itemoption_payment_person_persondiscount.py
+++ brambling/migrations/0002_eventhousing_eventperson_home_housingslot_itemoption_payment_person_persondiscount.py
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '__first__'),
+        ('contenttypes', '__first__'),
         ('brambling', '0001_initial'),
     ]
 
