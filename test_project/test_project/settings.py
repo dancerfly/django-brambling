d22a33dc3ae0f9ec0503be35564baabb5ae69962
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-05-19 22:58:33 -0700

    Explicitly set all formrow templates to avoid the {% extends %} tag. Seems to provide a slight increase in rendering speed.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -120,4 +120,20 @@ MESSAGE_TAGS = {
 if USE_DEBUG_TOOLBAR:
     INSTALLED_APPS += (
         'debug_toolbar.apps.DebugToolbarConfig',
+        'template_timings_panel',
     )
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+    ]
