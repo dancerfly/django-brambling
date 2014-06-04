45c4410e14ebc18ca573d1f5dfb38d9277f750bd
Harris Lapiroff <harris@chromamine.com>
2014-06-04 15:23:41 -0400

    Url /admin/ ends up getting captured too early if it's not first.

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('brambling.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('brambling.urls')),
 )
