79f8ff666d0b2695b6c0262d556c54d646b9236e
Harris Lapiroff <harris@chromamine.com>
2014-06-04 12:57:34 -0400

    Settings makes more sense than edit, I think.

--- brambling/views/utils.py
+++ brambling/views/utils.py
@@ -59,7 +59,7 @@ def get_event_admin_nav(event, request):
         return []
     items = (
         ('brambling_event_dashboard', 'Dashboard', 'glyphicon-dashboard', {'slug': event.slug}),
-        ('brambling_event_update', 'Edit', 'glyphicon-pencil', {'slug': event.slug}),
+        ('brambling_event_update', 'Settings', 'glyphicon-cog', {'slug': event.slug}),
         ('brambling_item_list', 'Items', 'glyphicon-list', {'event_slug': event.slug}),
         ('brambling_discount_list', 'Discounts', 'glyphicon-usd', {'event_slug': event.slug}),
     )
