b5fa5ac59197efc2e30d08fbc6ea955e2a7309b1
Harris Lapiroff <harris@chromamine.com>
2014-06-08 12:17:52 -0400

    Added some actions to the events page countdown timer:
    * Color changes as timer approaches 0.
    * When timer is completed, a modal pops up instructing user to
      start over from the shop screen.
    
    Resolves #69.

--- brambling/static/brambling/brambling.kickoff.js
+++ brambling/static/brambling/brambling.kickoff.js
@@ -31,6 +31,38 @@ $.ajaxSetup({
 });
 
 $(function() {
+
+    // Prevent disabled links from doing anything.
     $('.disabled a').on('click', function(e){e.preventDefault();});
+
+    // Add tooltips to elements with class "tipped".
     $('.tipped').tooltip();
+
+    // Bind some brambling-specific events to the countdown timer.
+    $('[data-countdown="timer"]').on("updated.countdown", function (e, data) {
+        var alert,
+            alert_levels = [
+                "alert-info",
+                "alert-warning",
+                "alert-danger"
+            ];
+        // Give different colors at < 3 and < 1 minute left.
+        if (data.minutes < 3 && data.minutes > 1) {
+            alert = $(this).closest(".alert");
+            if (alert.hasClass(alert_levels[0])) alert.removeClass(alert_levels[0]).addClass(alert_levels[1]);
+        } else if (data.minutes < 1 && data.seconds > 0) {
+            alert = $(this).closest(".alert");
+            if (alert.hasClass(alert_levels[0])) alert.removeClass(alert_levels[0]).addClass(alert_levels[2]);
+            if (alert.hasClass(alert_levels[1])) alert.removeClass(alert_levels[1]).addClass(alert_levels[2]);
+        }
+    });
+    // Upon countdown completion, display a modal which cannot be dismissed.
+    $('[data-countdown="timer"]').on("completed.countdown", function (e) {
+        $("#countdownCompleteModal").modal({
+            keyboard: false,
+            show: true,
+            backdrop: "static"
+        });
+    });
+
 });
