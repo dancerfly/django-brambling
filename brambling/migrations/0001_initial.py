25aeb96444339a7a0d440310df10c9a43855b10a
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-06-22 08:29:11 -0400

    Corrected new initial migration dependencies.

--- brambling/migrations/0001_initial.py
+++ brambling/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0002_auto_20140622_1221'),
+        ('auth', '__first__'),
     ]
 
     operations = [
