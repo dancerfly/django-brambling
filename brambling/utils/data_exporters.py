d565233087cbec9d69c827476a3cbd446c39424f
Harris Lapiroff <harris@chromamine.com>
2014-09-05 22:15:28 -0400

    Added a million fields to the AttendeeCSVExporter

--- brambling/utils/data_exporters.py
+++ brambling/utils/data_exporters.py
@@ -1,9 +1,23 @@
 import csv
 import io
+import itertools
 
 from django import forms
 
 __all__ = ('BaseModelCSVExporter', 'AttendeeCSVExporter')
+
+
+def comma_separated_manager(attr_name):
+    """
+    Returns a function which takes a M2M manager on an object and
+    returns it as a comma separated string.
+
+    """
+
+    def inner(self, obj):
+        manager = getattr(obj, attr_name)
+        return ", ".join([unicode(x) for x in manager.all()])
+    return inner
 
 
 class BaseModelCSVExporter(object):
@@ -14,9 +28,9 @@ class BaseModelCSVExporter(object):
     It takes two arguments:
 
     1. The queryset to be exported.
-    2. The fields as a list of 2-tuples of the format
-       ("Field Verbose Name", "field_name").
-       `field_name` can be the name of an attribute on the model
+    2. The fields as a list of 3-tuples of the format
+       ("Field Verbose Name", "field_name", default), where default is True
+       or False. `field_name` can be the name of an attribute on the model
        or an attribute on the Exporter subclass.
 
     """
@@ -46,8 +60,10 @@ class BaseModelCSVExporter(object):
             fields = [field
                       for field in self.fields
                       if cleaned_data[field[1]] == True]
-            return fields
-        # Default to all fields:
+            # Only return a list of fields if it isn't empty:
+            if not fields == []:
+                return fields
+        # Otherwise default to all fields:
         return self.fields
 
     def get_queryset(self):
@@ -67,7 +83,13 @@ class BaseModelCSVExporter(object):
             meth = getattr(self, key)
             val = meth(obj)
         else:
-            raise AttributeError
+            error_dict = {
+                'attr': key,
+                'model': obj.__class__.__name__,
+                'exporter': self.__class__.__name__,
+            }
+            error_string = "{attr} does not exist as an attribute of {model} or {exporter}".format(**error_dict)
+            raise AttributeError(error_string)
 
         if callable(val):
             val = val()
@@ -79,15 +101,25 @@ class BaseModelCSVExporter(object):
 
     @property
     def form(self):
+        """
+        Returns a form of booleans for each field in FIELD_OPTIONS,
+        bound with self.data if is not None.
+
+        """
+
         if not hasattr(self, '_form'):
-            fields = {
-                name[1]: forms.BooleanField(label=name[0], required=False)
-                for name in self.FIELD_OPTIONS}
+            fields = {}
+            for field in self.FIELD_OPTIONS:
+                boolean_field = forms.BooleanField(label=field[0], required=False, initial=field[2])
+                fields.update({field[1]: boolean_field})
+
             Form = type(str('{}Form'.format(self.__class__.__name__)), (self.get_form_class(),), fields)
+
             if self.is_bound:
                 self._form = Form(self.data, prefix=self.form_prefix)
             else:
                 self._form = Form(prefix=self.form_prefix)
+
         return self._form
 
     def render(self):
@@ -112,18 +144,71 @@ class BaseModelCSVExporter(object):
 
 class AttendeeCSVExporter(BaseModelCSVExporter):
 
-    #: A list of all possible fields
-    FIELD_OPTIONS = (
-        ("ID", "pk"),
-        ("Name", "get_full_name"),
-        ("Given Name", "given_name"),
-        ("Surname", "surname"),
-        ("Middle Name", "middle_name"),
-        ("Pass Type", "pass_type"),
-        ("Pass Status", "pass_status"),
+    #: A list of ID related fields.
+    IDENTIFICATION_FIELD_OPTIONS = (
+        ("ID", "pk", True),
+        ("Name", "get_full_name", True),
+        ("Given Name", "given_name", True),
+        ("Surname", "surname", True),
+        ("Middle Name", "middle_name", True),
     )
 
+    #: A list of contact related fields.
+    CONTACT_FIELD_OPTIONS = (
+        ("Email Address", "email", True),
+        ("Phone Number", "phone", True),
+    )
+
+    #: A list of pass fields.
+    PASS_FIELD_OPTIONS = (
+        ("Pass Type", "pass_type", True),
+        ("Pass Status", "pass_status", True),
+    )
+
+    #: A list of housing related fields.
+    HOUSING_FIELD_OPTIONS = (
+        ("Housing Status", "housing_status", True),
+        ("Housing Nights", "housing_nights", True),
+        ("Housing Environment Preference", "housing_preferences", True),
+        ("Housing Environment Avoid", "environment_avoid", True),
+        ("Attendee May Cause/Do", "environment_cause", True),
+        ("Housing People Preference", "person_prefer", True),
+        ("Housing People Avoid", "person_avoid", True),
+        ("Other Housing Needs", "other_needs", True),
+    )
+
+    #: A list of order related fields.
+    ORDER_FIELD_OPTIONS = (
+        ("Order ID", "order_id", True),
+        ("Order Placed By", "order_placed_by", True),
+    )
+
+    #: A list of miscellaneous fields.
+    MISCELLANEOUS_FIELD_OPTIONS = (
+        ("Liability Waiver Signed", "liability_waiver", True),
+        ("Consent to be Photographed", "photo_consent", True),
+    )
+
+    FIELD_OPTIONS_BY_CATEGORY = (
+        IDENTIFICATION_FIELD_OPTIONS,
+        CONTACT_FIELD_OPTIONS,
+        PASS_FIELD_OPTIONS,
+        HOUSING_FIELD_OPTIONS,
+        ORDER_FIELD_OPTIONS,
+        MISCELLANEOUS_FIELD_OPTIONS,
+    )
+
+    #: A list of all possible display fields.
+    FIELD_OPTIONS = reduce(tuple.__add__, FIELD_OPTIONS_BY_CATEGORY)
+
     # Methods to be used as fields
+    def order_id(self, obj):
+        return obj.order.pk
+
+    def order_placed_by(self, obj):
+        person = obj.order.person
+        return "{} ({})".format(person.get_full_name(), person.email)
+
     def pass_type(self, obj):
         return "{}: {}".format(
             obj.event_pass.item_option.item.name,
@@ -131,3 +216,8 @@ class AttendeeCSVExporter(BaseModelCSVExporter):
 
     def pass_status(self, obj):
         return obj.event_pass.status
+
+    housing_nights = comma_separated_manager("nights")
+    housing_preferences = comma_separated_manager("housing_prefer")
+    environment_avoid = comma_separated_manager("ef_avoid")
+    environment_cause = comma_separated_manager("ef_cause")
