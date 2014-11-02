8e742ba02ede67302e7821fdb38b330dde9d17b9
Harris Lapiroff <harris@chromamine.com>
2014-11-02 13:22:06 -0500

    Added a "Welcome to Dancerfly!" hint.
    Resolves #221.

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^feedback/', include('zenaida.contrib.feedback.urls')),
+    url(r'^hints/', include('zenaida.contrib.hints.urls')),
     url(r'^', include('brambling.urls')),
 )
