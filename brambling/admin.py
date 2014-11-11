95c49317b2b975cfe09ad00203facbc113bc1baf
Harris Lapiroff <harris@chromamine.com>
2014-11-11 11:26:46 -0500

    Made change password form accessible from admin.

--- brambling/admin.py
+++ brambling/admin.py
@@ -40,7 +40,10 @@ class PersonChangeForm(forms.ModelForm):
     the user, but replaces the password field with admin's
     password hash display field.
     """
-    password = ReadOnlyPasswordHashField()
+    password = ReadOnlyPasswordHashField(help_text="""Raw passwords are not
+                    stored, so there is no way to see
+                    this user's password, but you can change the password
+                    using <a href=\"password/\">this form</a>.""")
 
     class Meta:
         model = Person
