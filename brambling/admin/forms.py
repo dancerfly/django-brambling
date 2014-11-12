f99b06da1b1467bf584fa5be390db7db50a57607
Harris Lapiroff <harris@chromamine.com>
2014-11-12 12:07:58 -0500

    Whoops tabs.

--- brambling/admin/forms.py
+++ brambling/admin/forms.py
@@ -3,49 +3,49 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from brambling.models import Person
 
 class PersonCreationForm(forms.ModelForm):
-	"""A form for creating new users. Includes all the required
-	fields, plus a repeated password."""
-	password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
-	class Meta:
-		model = Person
-		fields = (Person.USERNAME_FIELD,) + tuple(Person.REQUIRED_FIELDS)
+    class Meta:
+        model = Person
+        fields = (Person.USERNAME_FIELD,) + tuple(Person.REQUIRED_FIELDS)
 
-	def clean_password2(self):
-		# Check that the two password entries match
-		password1 = self.cleaned_data.get("password1")
-		password2 = self.cleaned_data.get("password2")
-		if password1 and password2 and password1 != password2:
-			raise forms.ValidationError("Passwords don't match")
-		return password2
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
 
-	def save(self, commit=True):
-		# Save the provided password in hashed format
-		user = super(PersonCreationForm, self).save(commit=False)
-		user.set_password(self.cleaned_data["password1"])
-		if commit:
-			user.save()
-		return user
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(PersonCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 
 class PersonChangeForm(forms.ModelForm):
-	"""A form for updating users. Includes all the fields on
-	the user, but replaces the password field with admin's
-	password hash display field.
-	"""
-	password = ReadOnlyPasswordHashField(help_text="""Raw passwords are not
-					stored, so there is no way to see
-					this user's password, but you can change the password
-					using <a href=\"password/\">this form</a>.""")
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField(help_text="""Raw passwords are not
+                    stored, so there is no way to see
+                    this user's password, but you can change the password
+                    using <a href=\"password/\">this form</a>.""")
 
-	class Meta:
-		model = Person
-		fields = ('email', 'password', 'is_active',)
+    class Meta:
+        model = Person
+        fields = ('email', 'password', 'is_active',)
 
-	def clean_password(self):
-		# Regardless of what the user provides, return the initial value.
-		# This is done here, rather than on the field, because the
-		# field does not have access to the initial value
-		return self.initial["password"]
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
