3b6f65dbfd845c33f5d5716695bf3f6fa82ef326
Harris Lapiroff <harris@chromamine.com>
2014-12-08 13:03:08 -0500

    Merging migrations.

---
+++ brambling/migrations/0054_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('brambling', '0053_event_check_address_2'),
+        ('brambling', '0053_auto_20141208_1753'),
+    ]
+
+    operations = [
+    ]
