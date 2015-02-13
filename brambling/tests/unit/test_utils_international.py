251a051be90fefca3386d257f252ec29c3f1395f
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-02-13 12:32:13 -0800

    Added postal code tests.

---
+++ brambling/tests/unit/test_utils_international.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from django.core.exceptions import ValidationError
+
+from brambling.utils.international import clean_postal_code
+
+
+class PostalCodeTestCase(TestCase):
+    def test_gb_postal__valid1(self):
+        clean_postal_code('GB', 'NW4 9BX')
+
+    def test_gb_postal__valid2(self):
+        clean_postal_code('GB', 'M32 8BR')
+
+    def test_gb_postal__valid3(self):
+        clean_postal_code('GB', 'M3 3BT')
+
+    def test_gb_postal__invalid1(self):
+        with self.assertRaises(ValidationError):
+            clean_postal_code('GB', '12345')
+
+    def test_uk_postal__valid1(self):
+        clean_postal_code('US', '16801')
+
+    def test_uk_postal__valid2(self):
+        clean_postal_code('US', '16801-2345')
+
+    def test_uk_postal__invalid1(self):
+        with self.assertRaises(ValidationError):
+            clean_postal_code('US', 'ABCDEF')
+
+    def test_unhandled_country(self):
+        clean_postal_code('12', ';[]32adf,,,')
