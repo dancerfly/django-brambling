29bd8e41f08d87bd05d58ced180e7b5afc777261
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-02-28 16:33:29 -0800

    Added ordering by code to order table view.

--- brambling/filters.py
+++ brambling/filters.py
@@ -1,6 +1,8 @@
 import copy
 
 from django.db import models
+from django.utils.text import capfirst
+from django.utils.translation import ugettext as _
 import django_filters
 import floppyforms.__future__ as forms
 from floppyforms.__future__.models import FORMFIELD_OVERRIDES
@@ -22,6 +24,28 @@ class FloppyFilterSet(django_filters.FilterSet):
         filter_ = super(FloppyFilterSet, cls).filter_for_field(f, name)
         filter_.field_class = FILTER_FIELD_OVERRIDES[f.__class__]['form_class']
         return filter_
+
+    def get_ordering_field(self):
+        # Copied from django_filter. But in this context, uses floppyforms.
+        if self._meta.order_by:
+            if isinstance(self._meta.order_by, (list, tuple)):
+                if isinstance(self._meta.order_by[0], (list, tuple)):
+                    # e.g. (('field', 'Display name'), ...)
+                    choices = [(f[0], f[1]) for f in self._meta.order_by]
+                else:
+                    choices = [(f, _('%s (descending)' % capfirst(f[1:])) if f[0] == '-' else capfirst(f))
+                               for f in self._meta.order_by]
+            else:
+                # add asc and desc field names
+                # use the filter's label if provided
+                choices = []
+                for f, fltr in self.filters.items():
+                    choices.extend([
+                        (fltr.name or f, fltr.label or capfirst(f)),
+                        ("-%s" % (fltr.name or f), _('%s (descending)' % (fltr.label or capfirst(f))))
+                    ])
+            return forms.ChoiceField(label="Ordering", required=False,
+                                     choices=choices)
 
 
 class AttendeeFilterSet(django_filters.FilterSet):
@@ -70,4 +94,5 @@ class OrderFilterSet(FloppyFilterSet):
         model = Order
         fields = ['providing_housing', 'send_flyers', 'status']
         form = forms.Form
+        order_by = ['code', '-code']
 OrderFilterSet.base_filters['status'].field_class = forms.MultipleChoiceField
