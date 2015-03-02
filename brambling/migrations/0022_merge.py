1911f97068b718b52c715df907b14f875e600b63
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-03-02 11:30:16 -0800

    Merged transaction migrations in.

---
+++ brambling/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('brambling', '0016_transaction_bought_items'),
+        ('brambling', '0021_auto_20150228_1832'),
+    ]
+
+    operations = [
+    ]
