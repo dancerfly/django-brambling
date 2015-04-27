71bca5f8668ae611884ed6cae248ef732ba1b322
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-04-27 11:02:51 -0700

    Combined splash & dashboard pages; made call to action "Learn more" instead of "Sign up".

--- brambling/views/utils.py
+++ brambling/views/utils.py
@@ -18,14 +18,6 @@ def get_event_or_404(slug):
     return get_object_or_404(qs, slug=slug)
 
 
-def split_view(test, if_true, if_false):
-    # Renders if_true view if condition is true, else if_false
-    def wrapped(request, *args, **kwargs):
-        view = if_true if test(request, *args, **kwargs) else if_false
-        return view(request, *args, **kwargs)
-    return wrapped
-
-
 def route_view(test, if_true, if_false):
     # Redirects to if_true lazy reversal if condition is true and if_false
     # otherwise.
