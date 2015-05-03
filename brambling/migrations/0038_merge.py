1b19011a999e77c7987872262298730e9d905c67
Harris Lapiroff <harris@chromamine.com>
2015-05-03 14:43:05 -0400

    Migration merge.

---
+++ brambling/migrations/0038_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('brambling', '0032_remove_order_status'),
+        ('brambling', '0037_merge'),
+    ]
+
+    operations = [
+    ]
