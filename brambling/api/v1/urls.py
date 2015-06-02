7a7828d2f4df0e722828232eee0fea6f07a20ead
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-06-02 13:31:56 -0700

    Alphabetized viewset registration.

--- brambling/api/v1/urls.py
+++ brambling/api/v1/urls.py
@@ -19,19 +19,19 @@ from brambling.api.v1.views import (
 
 
 router = DefaultRouter()
-router.register('housingcategory', HousingCategoryViewSet)
-router.register('environmentalfactor', EnvironmentalFactorViewSet)
-router.register('dancestyle', DanceStyleViewSet)
-router.register('organization', OrganizationViewSet)
-router.register('event', EventViewSet)
 router.register('attendee', AttendeeViewSet)
-router.register('eventhousing', EventHousingViewSet)
-router.register('order', OrderViewSet)
 router.register('boughtitem', BoughtItemViewSet)
+router.register('dancestyle', DanceStyleViewSet)
+router.register('environmentalfactor', EnvironmentalFactorViewSet)
+router.register('event', EventViewSet)
+router.register('eventhousing', EventHousingViewSet)
+router.register('housingcategory', HousingCategoryViewSet)
 router.register('item', ItemViewSet)
 router.register('itemimage', ItemImageViewSet)
 router.register('itemoption', ItemOptionViewSet)
+router.register('order', OrderViewSet)
 router.register('orderdiscount', OrderDiscountViewSet)
+router.register('organization', OrganizationViewSet)
 
 
 urlpatterns = patterns('',
