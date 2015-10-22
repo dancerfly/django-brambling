0d245c13b03c76ba0b893c44b86189474baff999
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-10-21 18:23:11 -0700

    Added VCR support for more consistent dwolla API tests (so we don't hit the api every time / don't have to worry about expiring tokens constantly.

--- brambling/tests/integration/test_dwolla.py
+++ brambling/tests/integration/test_dwolla.py
@@ -1,13 +1,16 @@
 from decimal import Decimal
+import os
 
 from django.conf import settings
 from django.test import TestCase
 from dwolla import transactions
+import vcr
 
 from brambling.models import Event, Transaction
 from brambling.tests.factories import EventFactory, PersonFactory, OrderFactory
 from brambling.utils.payment import dwolla_prep, dwolla_charge, dwolla_refund, dwolla_get_token
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 CHARGE_DATA = {
     u'Amount': 42.15,
@@ -38,6 +41,7 @@ CHARGE_DATA = {
 
 
 class DwollaChargeTestCase(TestCase):
+    @vcr.use_cassette(os.path.join(CURRENT_DIR, 'fixtures/test_dwolla_charge__user.yaml'))
     def test_dwolla_charge__user(self):
         event = EventFactory(api_type=Event.TEST,
                              application_fee_percent=Decimal('2.5'))
