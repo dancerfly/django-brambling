0865ed537a7997eddbf5a136836f3993fee8bfe9
Stephen Burrows <stephen.r.burrows@gmail.com>
2015-10-24 11:04:59 -0700

    Hypothetical travis-ci support.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -81,12 +81,24 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('DB') == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'brambling_test',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Uncomment to disable migrations temporarily, i.e. for tests
 # Source: https://gist.github.com/NotSqrt/5f3c76cd15e40ef62d09
