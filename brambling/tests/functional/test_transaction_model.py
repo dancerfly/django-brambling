e7744a03947f2a9730cecd799ac95ae827d8ad6c
Harris Lapiroff <harris@chromamine.com>
2016-05-16 10:45:18 -0400

    Update zero amount transaction test

--- brambling/tests/functional/test_transaction_model.py
+++ brambling/tests/functional/test_transaction_model.py
@@ -2,9 +2,10 @@ from decimal import Decimal
 
 from django.test import TestCase
 
-from brambling.models import Transaction
+from brambling.models import Transaction, BoughtItem
 from brambling.tests.factories import (
-    TransactionFactory,
+    TransactionFactory, ItemFactory, ItemOptionFactory, EventFactory,
+    PersonFactory, OrderFactory
 )
 
 
@@ -47,28 +48,23 @@ class TransactionModelTestCase(TestCase):
         self.assertEqual(txn.get_refundable_amount(), Decimal("0.00"))
         self.assertEqual(txn.related_transaction_set.count(), 1)
 
-    def test_refund__zero_amount(self):
+    def test_refund__zero_amount_with_item(self):
+        person = PersonFactory()
+        event = EventFactory()
+        item = ItemFactory(event=event, name='Multipass')
+        order = OrderFactory(event=event, person=person)
+        item_option = ItemOptionFactory(price=100, item=item, name='Gold')
+        bought_item = BoughtItem.objects.create(item_option=item_option, order=order, price=Decimal(0), status=BoughtItem.BOUGHT)
         txn = TransactionFactory(amount=Decimal("0"))
-        self.assertEqual(txn.get_refundable_amount(), Decimal("0.00"))
-        self.assertEqual(txn.related_transaction_set.count(), 0)
+        txn.bought_items.add(bought_item)
+
+        self.assertEqual(txn.get_refundable_amount(), Decimal("0"))
+        self.assertEqual(txn.get_returnable_items().get(), bought_item)
+
         refund = txn.refund()
         self.assertIsInstance(refund, Transaction)
         self.assertEqual(txn.get_refundable_amount(), Decimal("0.00"))
-        self.assertEqual(txn.related_transaction_set.count(), 1)
-
-    def test_refund__zero_amount__refunded(self):
-        txn = TransactionFactory(amount=Decimal("0"))
-        self.assertEqual(txn.get_refundable_amount(), Decimal("0.00"))
-        self.assertEqual(txn.related_transaction_set.count(), 0)
-        refund = txn.refund()
-        self.assertIsInstance(refund, Transaction)
-        self.assertEqual(txn.get_refundable_amount(), Decimal("0.00"))
-        self.assertEqual(txn.related_transaction_set.count(), 1)
-
-        refund = txn.refund()
-        self.assertIsNone(refund)
-        self.assertEqual(txn.get_refundable_amount(), Decimal("0.00"))
-        self.assertEqual(txn.related_transaction_set.count(), 1)
+        self.assertEqual(len(txn.get_returnable_items()), 0)
 
     def test_unconfirmed_check(self):
         t = Transaction(is_confirmed=False, method=Transaction.CHECK)
