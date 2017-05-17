c6aeb8770ab1926769008f44b57e3204654ceab9
Emily Horsman <me@emilyhorsman.com>
2017-05-17 10:56:14 -0700

    Refactor TransferView tests

--- brambling/tests/functional/test_order_views.py
+++ brambling/tests/functional/test_order_views.py
@@ -100,9 +100,7 @@ class TransferViewTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
-    def test_transfer_item(self):
-        """A user should be able to transfer an item on their order to an
-        email address."""
+    def set_up_view(self, orderer=None, is_confirmed=False):
         organization = OrganizationFactory(check_payment_allowed=True)
         event_owner = PersonFactory()
         OrganizationMember.objects.create(
@@ -118,14 +116,13 @@ class TransferViewTestCase(TestCase):
         item = ItemFactory(event=event)
         item_option = ItemOptionFactory(price=100, item=item)
 
-        orderer = PersonFactory()
         receiver = PersonFactory()
-        order = OrderFactory(
-            event=event,
-            person=orderer,
-        )
+        order_kwargs = dict(event=event)
+        if orderer:
+            order_kwargs['person'] = orderer
+        order = OrderFactory(**order_kwargs)
         order.add_to_cart(item_option)
-        transaction = TransactionFactory(event=event, is_confirmed=True)
+        transaction = TransactionFactory(event=event, is_confirmed=is_confirmed)
         order.mark_cart_paid(transaction)
 
         # The BoughtItem should be in the correct state if we've set up this
@@ -141,11 +138,20 @@ class TransferViewTestCase(TestCase):
             bought_item=order.bought_items.first().pk,
             email=receiver.email,
         ))
-        view.request.user = orderer
+        view.request.user = orderer if orderer else AnonymousUser()
         view.event = event
         view.order = order
         view.workflow = RegistrationWorkflow(order=order, event=event)
         view.current_step = view.workflow.steps.get(view.current_step_slug)
+        return view
+
+    def test_transfer_item(self):
+        """A user should be able to transfer an item on their order to an
+        email address."""
+        view = self.set_up_view(
+            orderer=PersonFactory(),
+            is_confirmed=True,
+        )
         view.post(view.request)
 
         self.assertEqual(len(mail.outbox), 1)
@@ -155,98 +161,21 @@ class TransferViewTestCase(TestCase):
         """Only authenticated users should be allowed to transfer items,
         despite unauthenticated visitors being alloewd to place orders.
         """
-        organization = OrganizationFactory(check_payment_allowed=True)
-        event_owner = PersonFactory()
-        OrganizationMember.objects.create(
-            person=event_owner,
-            organization=organization,
-            role=OrganizationMember.OWNER,
+        view = self.set_up_view(
+            orderer=None,
+            is_confirmed=True,
         )
-        event = EventFactory(
-            collect_housing_data=False,
-            organization=organization,
-            check_postmark_cutoff=timezone.now().date() + timedelta(1),
-        )
-        item = ItemFactory(event=event)
-        item_option = ItemOptionFactory(price=100, item=item)
-
-        receiver = PersonFactory()
-        order = OrderFactory(event=event)
-        order.add_to_cart(item_option)
-        transaction = TransactionFactory(event=event, is_confirmed=True)
-        order.mark_cart_paid(transaction)
-
-        # The BoughtItem should be in the correct state if we've set up this
-        # test Order correctly.
-        self.assertEqual(order.bought_items.first().status, BoughtItem.BOUGHT)
-
-        view = TransferView()
-        view.kwargs = dict(
-            event_slug=event.slug,
-            organization_slug=organization.slug,
-        )
-        view.request = self.factory.post('/', dict(
-            bought_item=order.bought_items.first().pk,
-            email=receiver.email,
-        ))
-        view.request.user = AnonymousUser()
-        view.event = event
-        view.order = order
-        view.workflow = RegistrationWorkflow(order=order, event=event)
-        view.current_step = view.workflow.steps.get(view.current_step_slug)
         response = view.post(view.request)
+
         self.assertEqual(response.status_code, 302)
         self.assertEqual(len(mail.outbox), 0)
 
     def test_pending_payment_transfer_fails(self):
         """A transfer on an item with unconfirmed transactions should be prohibited."""
-        organization = OrganizationFactory(check_payment_allowed=True)
-        event_owner = PersonFactory()
-        OrganizationMember.objects.create(
-            person=event_owner,
-            organization=organization,
-            role=OrganizationMember.OWNER,
-        )
-        event = EventFactory(
-            collect_housing_data=False,
-            organization=organization,
-            check_postmark_cutoff=timezone.now().date() + timedelta(1),
-        )
-        item = ItemFactory(event=event)
-        item_option = ItemOptionFactory(price=100, item=item)
-
-        orderer = PersonFactory()
-        receiver = PersonFactory()
-        order = OrderFactory(
-            event=event,
-            person=orderer,
-        )
-        order.add_to_cart(item_option)
-        transaction = TransactionFactory(
-            event=event,
-            # A non-confirmed transaction should prevent a transfer.
+        view = self.set_up_view(
+            orderer=PersonFactory(),
             is_confirmed=False,
         )
-        order.mark_cart_paid(transaction)
-
-        # The BoughtItem should be in the correct state if we've set up this
-        # test Order correctly.
-        self.assertEqual(order.bought_items.first().status, BoughtItem.BOUGHT)
-
-        view = TransferView()
-        view.kwargs = dict(
-            event_slug=event.slug,
-            organization_slug=organization.slug,
-        )
-        view.request = self.factory.post('/', dict(
-            bought_item=order.bought_items.first().pk,
-            email=receiver.email,
-        ))
-        view.request.user = orderer
-        view.event = event
-        view.order = order
-        view.workflow = RegistrationWorkflow(order=order, event=event)
-        view.current_step = view.workflow.steps.get(view.current_step_slug)
         view.post(view.request)
 
         self.assertEqual(len(mail.outbox), 0)
