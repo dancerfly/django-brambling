a4be5519875f128456853c3c8beccbf997a53259
Stephen Burrows <stephen.r.burrows@gmail.com>
2018-03-16 16:26:26 -0700

    Added serving of static and media files on dev

--- dancerfly_project/urls.py
+++ dancerfly_project/urls.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^talkback/', include('talkback.urls')),
     url(r'^hints/', include('zenaida.contrib.hints.urls')),
     url(r'^', include('brambling.urls')),
 )
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns = staticfiles_urlpatterns() + urlpatterns
+    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
