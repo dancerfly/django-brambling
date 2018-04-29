747de2aaf1ef9f9f3389aa614d2cb084259ac8c3
Stephen Burrows <stephen.r.burrows@gmail.com>
2018-04-29 16:53:29 -0700

    Turned off DEBUG in production

--- dancerfly_project/settings/production.py
+++ dancerfly_project/settings/production.py
@@ -2,6 +2,7 @@ import os
 
 from .base import *  # noqa: F403, F401
 
+DEBUG = False
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', '')
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', '')
