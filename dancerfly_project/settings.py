4206fbdd85d4d5515b444485463faa75c3da1cb0
Harris Lapiroff <harris@chromamine.com>
2018-06-20 22:30:12 -0400

    Add analytics context processor

--- dancerfly_project/settings.py
+++ dancerfly_project/settings.py
@@ -88,6 +88,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
     'brambling.context_processors.current_site',
+    'brambling.context_processors.google_analytics',
 )
 
 AUTHENTICATION_BACKENDS = [
