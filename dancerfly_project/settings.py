0f57ff926acf16063695788dcee91671b2ed5de3
Harris Lapiroff <harris@chromamine.com>
2018-06-20 22:24:08 -0400

    Add ADMINS setting

--- dancerfly_project/settings.py
+++ dancerfly_project/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
+ADMINS = [('Dancerfly Support', 'support@dancerfly.com'),]
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'NOT_SECRET')
 
